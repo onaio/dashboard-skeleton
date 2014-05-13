ff9908c5ee87a2e2e8e9bd7166e386423f05bb10
Larry Weya <larryweya@gmail.com>
2014-05-13 13:12:32 +0300

    LW: commit Configurator to allow access to ninja environment before adding filters

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -47,6 +47,7 @@ def main(global_config, **settings):
 def includeme(config):
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path("dashboard:templates")
+    config.commit()  # commit to allow access to jinja environment
     config.add_static_view('static', 'dashboard:static', cache_max_age=3600)
     config.add_route('default', '/')
     config.add_route(
