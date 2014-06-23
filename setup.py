de640f93e245c10c7ed71b9d57c6ef4177310344
Larry Weya <larryweya@gmail.com>
2014-06-23 13:05:55 +0300

    LW: bumped version to 0.1.2d

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requires = [
 ]
 
 setup(name='Dashboard',
-      version='0.1.2c',
+      version='0.1.2d',
       description='Dashboard',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
